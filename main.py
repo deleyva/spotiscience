993f61ca63e4a107f9f93f1a89a4d6d504d2864f
deleyva <jesuslopezdeleyva@gmail.com>
2022-02-21 14:16:26 +0100

    actualizo dependencias

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from spotiscience.credentials import CREDENTIALS
 import spotiscience.downloader as downloader
 import json
+import pyperclip
+
 
 sd = downloader.SpotiScienceDownloader(credentials=CREDENTIALS)
-song_copy_link = (
-    "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b?si=369f90167c9d48fb"
-)
+song_copy_link = pyperclip.paste()
 song = sd.get_song_features(song_id=song_copy_link)
 print(json.dumps(song, indent=4))
 
